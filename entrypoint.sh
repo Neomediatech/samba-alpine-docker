cd62ac1dc47cb18bed7b6e910db8628a9902a5fe
Dario B <feddar@users.noreply.github.com>
2019-03-13 17:37:08 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,24 @@
-    sed -i 's|^\( *workgroup = \).*|\1'"$workgroup"'|' $file
+#!/bin/bash
+
+
+
+
+sed -i 's|^\( *workgroup = \).*|\1'"$workgroup"'|' $file
+
+if [ ! -d /data/logs ]; then
+  mkdir -p /data/logs
+fi
+
+LOGFILE="/data/logs/samba.log"
+
+if [ ! -d /data ]; then
+  mkdir -p /data
+fi
+
+if [ ! -f $LOGFILE ]; then
+  touch $LOGFILE 
+  chmod 666 $LOGFILE
+fi
+
+exec tail -f $LOGFILE &
+exec "$@"
